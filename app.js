e72f07683150f3e04d19669e07bfb4aa64083930
kingleychuks1 <kingleychuks@gmail.com>
2024-01-28 18:45:14 +0000

    Updated comment for easy understanding

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(function (req, res, next) {
     });
 });
 
-
+// Mongodb url let
 const {MongoClient} = require("mongodb");
 const ObjectID = require('mongodb').ObjectId;
 const uri = "mongodb+srv://WebstoreUser:webstorecw2@webstorecluster0.fek9jms.mongodb.net/Webstore?retryWrites=true&w=majority"
