2a161af1edd81587a1df87dbab40382f4df01d46
kinglechuks1 <kingleychuks@gmail.com>
2024-03-10 15:47:38 +0000

    changes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(function (req, res, next) {
     console.log("Request URL: " + req.url);
     console.log("Request Date: " + new Date());
-    next();
+    next()
 });
 
 //File Handler Middleware
